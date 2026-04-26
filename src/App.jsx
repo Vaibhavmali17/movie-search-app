f4d56578216b4bbd331ea8b08ffec996a3b6b0e1
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-04-26 23:16:16 +0530

    Day 1: Initial setup with React Router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MovieDetail from './pages/MovieDetail';
+import Navbar from './components/Navbar';
+
+function App  ()  {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
